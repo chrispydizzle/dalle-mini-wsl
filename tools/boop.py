377462bcb513d2885d2ec82b5544e3d8760cc44a
Chris <posada.chris@gmail.com>
2022-06-29 01:16:33 -0400

    stuff

---
+++ tools/boop.py
@@ -0,0 +1,34 @@
+import jax
+import jax.numpy as jnp
+from flax.jax_utils import replicate
+from dalle_mini import DalleBart, DalleBartProcessor
+from vqgan_jax.modeling_flax_vqgan import VQModel
+from transformers import CLIPProcessor, FlaxCLIPModel
+# Model references
+
+# dalle-mega
+DALLE_MODEL = "dalle-mini/dalle-mini/mega-1-fp16:latest"  # can be wandb artifact or 🤗 Hub or local folder or google bucket
+DALLE_COMMIT_ID = None
+
+# if the notebook crashes too often you can use dalle-mini instead by uncommenting below line
+# DALLE_MODEL = "dalle-mini/dalle-mini/mini-1:v0"
+
+# VQGAN model
+VQGAN_REPO = "dalle-mini/vqgan_imagenet_f16_16384"
+VQGAN_COMMIT_ID = "e93a26e7707683d349bf5d5c41c5b0ef69b677a9"
+
+# check how many devices are available
+jax.local_device_count()
+
+# Load dalle-mini
+model, params = DalleBart.from_pretrained(
+    DALLE_MODEL, revision=DALLE_COMMIT_ID, dtype=jnp.float16, _do_init=False
+)
+
+# Load VQGAN
+vqgan, vqgan_params = VQModel.from_pretrained(
+    VQGAN_REPO, revision=VQGAN_COMMIT_ID, _do_init=False
+)
+
+params = replicate(params, [ jax.devices('gpu')[0] ])
+vqgan_params = replicate(vqgan_params, jax.devices('gpu')[0])
